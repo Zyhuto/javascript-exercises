cc0f937e9bbeda5827c5c585e946d3d98c7ad0de
Zyhuto <119459963+Zyhuto@users.noreply.github.com>
2024-08-07 23:11:24 +0200

    Finish Javascript Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number.parseInt(n);
+    if (Number.isNaN(n) || n < 0) {return "OOPS";}
+    if (n == 0) {return 0;}
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    let fibnum1 = 1;
+    let fibnum2 = 1;
+    for (i = 1; i <= (n - 2); i++) {
+        [fibnum2, fibnum1] = [fibnum1, fibnum1 + fibnum2];
+    }
+    return fibnum1;
 
 };
 
