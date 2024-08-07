c342b39118683c8b08fa6a9422252fba21e1c579
Zyhuto <119459963+Zyhuto@users.noreply.github.com>
2024-08-07 22:51:52 +0200

    Finish Javascript Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let filterSet = new Set([" ",",",".",";","!","9"])
+    let stringArray = Array.from(string.toLowerCase())
+    let filteredArray = stringArray.filter(x => !filterSet.has(x))
+    return filteredArray.join("") == filteredArray.reverse().join("");
 
 };
 
