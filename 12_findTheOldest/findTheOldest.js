63784ab06206ec4b8c5f421bdab7ce7df96df862
Zyhuto <119459963+Zyhuto@users.noreply.github.com>
2024-08-07 23:34:38 +0200

    Finish Javascript Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(personsArray) {
+    return personsArray.reduce((person1, person2) => oldestReducer(person1, person2));
 
 };
 
+let currentYear = new Date().getFullYear();
+
+function oldestReducer (prev, current) {
+    if(getAge(prev,currentYear) > getAge(current,currentYear)) {
+        return prev;
+    } else {
+        return current;
+    }
+
+}
+
+function getAge(person, currentYear) {
+    if (person.yearOfDeath == undefined) {
+        return currentYear - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
+
+
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+
